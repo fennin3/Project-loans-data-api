97aa829a96a2c6183ba9fb4c8f93c27927527dff
Francis Ennin <enninfrancis47@gmail.com>
2023-01-08 09:36:57 +0000

    Configured dateformat

--- loan_data_project/settings.py
+++ loan_data_project/settings.py
@@ -124,6 +124,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = False
+
+DATE_FORMAT = "%d %B %Y"
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
