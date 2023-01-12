b9859c4c41558b4a381433496389d8ab56791035
Francis Ennin <enninfrancis47@gmail.com>
2023-01-12 00:50:54 +0000

    registered new models in the admin

--- loan_app/admin.py
+++ loan_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Loan
+from .models import Loan, Country, Currency, Sector
 
 admin.site.register(Loan)
+admin.site.register(Country)
+admin.site.register(Currency)
+admin.site.register(Sector)
