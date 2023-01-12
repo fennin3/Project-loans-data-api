b52d80a52895f125c4813fab3a8e84084cc877dc
Francis Ennin <enninfrancis47@gmail.com>
2023-01-12 00:49:48 +0000

    serializers for the views

--- loan_app/serializers.py
+++ loan_app/serializers.py
@@ -1,8 +1,38 @@
 from rest_framework.serializers import ModelSerializer
+from .models import Loan, Country, Sector, Currency
 
-from .models import Loan
+
+class CountrySerializer(ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ("name",)
+
+
+class SectorSerializer(ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = ("name",)
+
+
+class CurrencySerializer(ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = ("symbol",)
+
 
 class LoanSerializer(ModelSerializer):
+    country = CountrySerializer()
+    sector = SectorSerializer()
+    currency = CurrencySerializer()
+
     class Meta:
-        model=Loan
-        fields="__all__"
+        model = Loan
+        fields = (
+            "uid",
+            "title",
+            "signed_amount",
+            "signature_date",
+            "country",
+            "sector",
+            "currency",
+        )
