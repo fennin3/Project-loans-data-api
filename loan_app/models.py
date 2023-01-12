4b831f6e34d59c4300df1fd8bacb7f6b4b612ce5
Francis Ennin <enninfrancis47@gmail.com>
2023-01-12 00:50:20 +0000

    added 3 new models

--- loan_app/models.py
+++ loan_app/models.py
@@ -1,15 +1,45 @@
 from django.db import models
 import uuid
 
+
+class Country(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Sector(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    symbol = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.symbol
+
+    class Meta:
+        verbose_name_plural = "Currencies"
+
+
 class Loan(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    sector = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
     signed_amount = models.DecimalField(max_digits=11, decimal_places=2)
     signature_date = models.DateField()
-    
+
     def __str__(self):
         return self.title
-    
-    
