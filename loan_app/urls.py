b628b51ba8fe1cd0ab0c89c01f349cdd7b1ae660
Francis Ennin <enninfrancis47@gmail.com>
2023-01-12 00:48:54 +0000

    Added url path for the excel generation view

--- loan_app/urls.py
+++ loan_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
-    path("countries",views.list_countries, name="list_all_countries"),
-    path("sectors",views.list_sectors, name="list_all_sectors"),
-    path("projects",views.list_project_titles, name="list_all_titles"),
-    path("loans",views.ListAllLoans.as_view(), name="list_all_loans"),
+    path("countries", views.list_countries, name="list_all_countries"),
+    path("sectors", views.list_sectors, name="list_all_sectors"),
+    path("projects", views.list_project_titles, name="list_all_titles"),
+    path("loans", views.ListAllLoans.as_view(), name="list_all_loans"),
+    path("excel", views.generate_excel_records, name="generate_excel"),
 ]
